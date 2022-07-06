a7dc368893a59a66d07a4ee025456f4ed0c72a9a
ahsanul-hub <aldi.madridista.am@gmail.com>
2022-07-06 14:16:05 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
   ];
 
   return (
-    <div className="App">
-      <h1>Daftar Makanan</h1>
+    <div className="App py-3">
+      <h1 className="mt-4">Daftar Makanan</h1>
       <Container>
         <Row>
           <Col>
-            <Table striped bordered hover>
+            <Table striped bordered hover className="mt-4">
               <thead>
                 <tr>
                   <th>ID</th>
@@ -30,6 +30,38 @@ function App() {
                     <td>{item.price}</td>
                   </tr>
                 ))}
+                <tr>
+                  <td colspan="2" className="text-start px-4">
+                    Total :
+                  </td>
+                  <td>{data.reduce((sum, elem) => sum + elem.price, 0)}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Table className="mt-4">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nama Makanan</th>
+                  <th>Harga</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item, idx) =>
+                  item.price >= 12500 ? (
+                    <tr>
+                      <td>{idx + 1}</td>
+                      <td>{item.title}</td>
+                      <td>{item.price}</td>
+                    </tr>
+                  ) : (
+                    <></>
+                  )
+                )}
               </tbody>
             </Table>
           </Col>
